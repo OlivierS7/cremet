740a4eeaeac077c05cdbbe6968c5c44d40d24337
OlivierS7 <olivier.simonneau@viacesi.fr>
2020-11-11 13:52:42 +0100

    Background fixed

--- js/carousel-3d.js
+++ js/carousel-3d.js
@@ -4,23 +4,45 @@ let cellCount = 5;
 let selectedIndex = 0;
 let cellWidth;
 let radius, theta, angle;
+let counter = 0;
 
 function changeCarousel() {
-    angle = theta * selectedIndex * -1;
+    if (selectedIndex > 4) {
+        selectedIndex = 0;
+    } else if (selectedIndex < 0) {
+        selectedIndex = 4;
+    }
+    let nextSlide = selectedIndex + 1;
+    let previousSlide = selectedIndex - 1;
+    if (nextSlide > 4) {
+        nextSlide = 0;
+    }
+    if (previousSlide < 0) {
+        previousSlide = 4;
+    }
+    angle = theta * counter * -1;
     carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
         'rotateY(' + angle + 'deg)';
+    for (let i = 0; i < cells.length; i++) {
+        let cell = cells[i];
+        if (i == nextSlide || i == previousSlide || i == selectedIndex) {
+            cell.style.opacity = 1;
+        } else {
+            cell.style.opacity = 0;
+        }
+    }
 }
 
 let prevButton = document.querySelector('.previous-button');
 prevButton.addEventListener('click', function() {
-    selectedIndex--;
-    changeCarousel();
+    counter--;
+    changeCarousel(selectedIndex--);
 });
 
 let nextButton = document.querySelector('.next-button');
 nextButton.addEventListener('click', function() {
-    selectedIndex++;
-    changeCarousel();
+    counter++;
+    changeCarousel(selectedIndex++);
 });
 
 
@@ -28,8 +50,8 @@ function startCarousel() {
     theta = 360 / cellCount;
     cellWidth = carousel.offsetWidth;
     radius = Math.round((cellWidth / 2) / Math.tan(Math.PI / cellCount));
-    for (var i = 0; i < cells.length; i++) {
-        var cell = cells[i];
+    for (let i = 0; i < cells.length; i++) {
+        let cell = cells[i];
         if (i < cellCount) {
             cell.style.opacity = 1;
             let cellAngle = theta * i;
@@ -38,7 +60,7 @@ function startCarousel() {
             cell.style.transform = 'none';
         }
     }
-    changeCarousel();
+    changeCarousel(1);
 }
 startCarousel();
 
@@ -46,11 +68,10 @@ startCarousel();
 window.addEventListener("resize", function() {
     cellWidth = carousel.offsetWidth;
     radius = Math.round((cellWidth / 2) / Math.tan(Math.PI / cellCount));
-    console.log("test");
     carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
         'rotateY(' + angle + 'deg)';
-    for (var i = 0; i < cells.length; i++) {
-        var cell = cells[i];
+    for (let i = 0; i < cells.length; i++) {
+        let cell = cells[i];
         if (i < cellCount) {
             cell.style.opacity = 1;
             let cellAngle = theta * i;
---
+++ js/loadBackground.js
@@ -0,0 +1,4 @@
+let bg = new Image('../images/background.jpg');
+bg.onload = function() {
+    document.body.style.background = "url('../images/background.jpg') cover no-repeat center";
+}
